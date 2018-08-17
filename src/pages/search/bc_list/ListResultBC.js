3c370d572325387847cdaf7d2185577b017a431b
Marc Delerue <mdelerue@tetra-info.com>
2018-08-17 14:46:03 +0200

    swipe qui fonctionne à peu près

--- src/pages/search/bc_list/ListResultBC.js
+++ src/pages/search/bc_list/ListResultBC.js
@@ -19,55 +19,54 @@ import {
     Title
 } from "native-base";
 import React, { Component } from "react";
-import { SwipeListView } from 'react-native-swipe-list-view';
-import { View } from 'react-native';
+import { ListView, View } from 'react-native';
 
 import PropTypes from "prop-types";
 import _ from "lodash";
 
 export default class ListResultBC extends Component {
-
-
     constructor(props) {
         super(props);
+
+        this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     }
 
     
     render() {
         if (!!this.props.data && this.props.data.length > 0) {
-            return <SwipeListView
-                useFlatList
-                data={this.props.data}
-                renderItem={(rowData, rowMap) => (<ListItem thumbnail style={{paddingLeft: 10, paddingRight: 10}}>
+            return <List
+                disableLeftSwipe
+                leftOpenValue={75}
+                rightOpenValue={-75}
+                dataSource={this.ds.cloneWithRows(this.props.data)}
+                renderRow={(item) => (<ListItem thumbnail style={{paddingLeft: 10, paddingRight: 10}}>
                     <Left>
-                        {_.get(rowData.item, "thumb") && rowData.item.thumb.trim() !== "" 
-                            ? <Thumbnail square source={{ uri: rowData.item.thumb }} style={{width: 60, height: 60}} /> 
+                        {_.get(item, "thumb") && item.thumb.trim() !== "" 
+                            ? <Thumbnail square source={{ uri: item.thumb }} style={{width: 60, height: 60}} /> 
                             : <Icon name="md-image" style={{fontSize: 60, width: 60}} />}
                     </Left>
                     <Body>
-                        <Text>{rowData.item.title}</Text>
-                        <Text note>{rowData.item.type} - {rowData.item.year}</Text>
+                        <Text>{item.title}</Text>
+                        <Text note>{item.type} - {item.year}</Text>
                         <View style={{flexDirection: "row"}}>
                             <Text note style={{color: "black", flex: 1}}>Format :</Text>
-                            <Text note style={{flex: 3}} numberOfLines={3}>{_.chain(rowData.item.format).uniq().map(e => e).join(", ").value()}</Text>
+                            <Text note style={{flex: 3}} numberOfLines={3}>{_.chain(item.format).uniq().map(e => e).join(", ").value()}</Text>
                         </View>
                         <View style={{flexDirection: "row"}}>
                             <Text note style={{color: "black", flex: 1}}>Label :</Text>
-                            <Text note style={{flex: 3}} numberOfLines={3}>{_.chain(rowData.item.label).uniq().map(e => e).join(", ").value()}</Text>
+                            <Text note style={{flex: 3}} numberOfLines={3}>{_.chain(item.label).uniq().map(e => e).join(", ").value()}</Text>
                         </View>
                         <View style={{flexDirection: "row"}}>
                             <Text note style={{color: "black", flex: 1}}>BC :</Text>
-                            <Text note style={{flex: 3}} numberOfLines={3}>{_.chain(rowData.item.barcode).uniq().map(e => e).join(", ").value()}</Text>
+                            <Text note style={{flex: 3}} numberOfLines={3}>{_.chain(item.barcode).uniq().map(e => e).join(", ").value()}</Text>
                         </View>
                     </Body>
                     </ListItem>)    
                 }
-                renderHiddenItem={(data, rowMap) => (
-                    <View>
-                        <Text>Left</Text>
-                        <Text>Right</Text>
-                    </View>
-                )}
+                renderLeftHiddenRow={data =>
+                    <Button full onPress={() => alert(data)}>
+                      <Icon active name="information-circle" />
+                    </Button>}
             />;
 
         } else {
